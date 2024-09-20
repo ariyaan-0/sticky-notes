7f4a4dc180cb41a18c0a48eac820c55f2cce21fc
Md Rezaul Hossain <str.rezaul@gmail.com>
2024-09-21 01:19:18 +0600

    complete note taking app

--- readme.md
+++ readme.md
@@ -2,3 +2,5 @@ A basic note taking app.
 
 One can create, edit as well as delete notes. Note will have two part, one is the title and another part is the description or the main body of the note.
 A note card will save the date of the note.
+
+![note](images/note.png)
--- script.js
+++ script.js
@@ -5,23 +5,88 @@ const closeIcon = popupBox.querySelector("header i");
 const addBtn = popupBox.querySelector("button");
 const titleTag = popupBox.querySelector("input");
 const descTag = popupBox.querySelector("textarea");
+const popupTitle = popupBox.querySelector("header p");
 
 const months = ["Jan", "Feb", "March", "April", "May", "June", "July", "Aug", "Sep", "Nov", "Dec"];
+
+let isUpdate = false;
+let updateID;
 
 //getting local storate notes if exists and parsing them to js objec
 //else passing an empty array to store notes
 const notes = JSON.parse(localStorage.getItem("notes") || "[]");
 
 addBox.addEventListener("click", ()=>{
+    titleTag.focus()
     popupBox.classList.add("show");
 });
 
 closeIcon.addEventListener("click", ()=>{
+    isUpdate = false;
+    titleTag.value = "";
+    descTag.value= "";
+    addBtn.innerText = "Add Note";
+    popupTitle.innerText = "Add a new Note";
     popupBox.classList.remove("show");
 });
 
 function showNotes(){
-    
+    document.querySelectorAll(".note").forEach(note => note.remove());
+    notes.forEach((note, index)=>{
+        // console.log(note);
+        let liTag = `<li class="note">
+                        <div class="details">
+                            <p>${note.title}</p>
+                            <span>${note.description}</span>
+                        </div>
+                        <div class="bottom-content">
+                            <span>${note.date}</span>
+                            <div class="settings">
+                                <i onclick="showMenu(this)" class="uil uil-ellipsis-h"></i>
+                                <ul class="menu">
+                                    <li onclick="updateNote(${index}, '${note.title}', '${note.description}')"><i class="uil uil-pen"></i>Edit</li>
+                                    <li onclick="deleteNote(${index})"><i class="uil uil-trash"></i>Delete</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </li>`;
+
+        addBox.insertAdjacentHTML("afterend", liTag);
+    });
+
+}
+
+showNotes();
+
+function showMenu(elem){
+    elem.parentElement.classList.add("show");
+    document.addEventListener("click", e => {
+        if(e.target.tagName != "I" || e.target != elem){
+            elem.parentElement.classList.remove("show");
+        }
+    });
+}
+
+function deleteNote(noteID){
+    // console.log(noteID);
+    let confirmDel = confirm("Are you sure?");
+    if(!confirmDel) return;
+    //removing selected note
+    notes.splice(noteID, 1);
+    //Save notes to localstorage
+    localStorage.setItem("notes", JSON.stringify(notes));
+    showNotes();
+}
+
+function updateNote(noteID, title, desc){
+    // console.log(noteID, title, desc)
+    addBox.click()
+    isUpdate = true;
+    updateID = noteID;
+    titleTag.value = title;
+    descTag.value= desc;
+    addBtn.innerText = "Update Note";
+    popupTitle.innerText = "Update a Note";
 }
 
 addBtn.addEventListener("click", (e)=>{
@@ -44,10 +109,16 @@ addBtn.addEventListener("click", (e)=>{
 
         // console.log(noteInfo)
 
-        notes.push(noteInfo); //adding new notes.
+        if(!isUpdate){
+            notes.push(noteInfo); //adding new notes.
+        } else{
+            notes[updateID] = noteInfo;
+            isUpdate = false;
+        }
+        
         //Save notes to localstorage
         localStorage.setItem("notes", JSON.stringify(notes));
         closeIcon.click();
-
+        showNotes();
     }
 });
